80ffd8bf9b999bd116f5848097d1ec278d1ea81d
Michael Mahemoff <you@example.com>
2011-04-06 23:13:06 +0000

    swipe lib is now a more gen-purpose touch lib

--- server/client/scripts/phone/controller.js
+++ server/client/scripts/phone/controller.js
@@ -9,17 +9,18 @@ var PhoneController = function() {
     var categoryIndex = $(".category.active").prev().length;
     c=categoryIndex;
     $(".categories").css({marginLeft: -categoryIndex*$(window).width()});
-    $(".categories").swipe(function(ev) {
-      categoryIndex = 
-        ev.dx < 0 ? Math.min(categoryIndex+1, $(".category").length-1)
-                  : Math.max(categoryIndex-1, 0);
-      $(".categories").animate({marginLeft: -categoryIndex*$(window).width()});
-      log("swiped", ev.dx, categoryIndex);
-      $(".category").removeClass("active");
-      log("ci", categoryIndex);
-      $(".categories").eq(categoryIndex).addClass("active");
-      // controller.activate($(".categories").get(categoryIndex));
-    }, {
+    $(".categories").touch({
+      swipeX: function(ev) {
+        categoryIndex = 
+          ev.dx < 0 ? Math.min(categoryIndex+1, $(".category").length-1)
+                    : Math.max(categoryIndex-1, 0);
+        $(".categories").animate({marginLeft: -categoryIndex*$(window).width()});
+        log("swiped", ev.dx, categoryIndex);
+        $(".category").removeClass("active");
+        log("ci", categoryIndex);
+        $(".categories").eq(categoryIndex).addClass("active");
+        // controller.activate($(".categories").get(categoryIndex));
+      },
       move: function(ev) {
         log("moved", ev.dx);
         $(".categories").css("marginLeft", ev.dx + parseInt($(".categories").css("marginLeft")));

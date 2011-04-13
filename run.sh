27bbfadf408ae20ad4dd2f040f85b6e34d9877b7
Paul Kinlan <paulkinlan@google.com>
2011-04-13 15:23:02 +0100

    Adding Less to the build dependencies

--- run.sh
+++ run.sh
@@ -6,22 +6,23 @@ then
   # Compile the CSS
   cat ./server/client/css/base.less ./server/client/css/desktop/*.less > /tmp/io-reader.desktop.less 
   cat ./server/client/css/base.less ./server/client/css/phone/*.less > /tmp/io-reader.phone.less 
-  cat ./server/client/css/base.less ./server/client/css/table/*.less > /tmp/io-reader.tablet.less 
+  cat ./server/client/css/base.less ./server/client/css/tablet/*.less > /tmp/io-reader.tablet.less 
   cat ./server/client/css/base.less ./server/client/css/tv/*.less > /tmp/io-reader.tv.less 
+ 
+  (
+    cd ./build/less 
+    node lessc /tmp/io-reader.desktop.less > ../../server/client/css/desktop.css
+    node lessc /tmp/io-reader.desktop.less > ../../server/client-min/css/desktop.css
+  
+    node lessc /tmp/io-reader.phone.less > ../../server/client/css/phone.css
+    node lessc /tmp/io-reader.phone.less > ../../server/client-min/css/phone.css
 
-  lessc /tmp/io-reader.desktop.less > ./server/client/css/desktop.css
-  lessc /tmp/io-reader.desktop.less > ./server/client-min/css/desktop.css
+    node lessc /tmp/io-reader.tablet.less > ../../server/client/css/tablet.css
+    node lessc /tmp/io-reader.tablet.less > ../../server/client-min/css/tablet.css
   
-  lessc /tmp/io-reader.phone.less > ./server/client/css/phone.css
-  lessc /tmp/io-reader.phone.less > ./server/client-min/css/phone.css
-
-  lessc /tmp/io-reader.tablet.less > ./server/client/css/tablet.css
-  lessc /tmp/io-reader.tablet.less > ./server/client-min/css/tablet.css
-  
-  lessc /tmp/io-reader.tv.less > ./server/client/css/tv.css
-  lessc /tmp/io-reader.tv.less > ./server/client-min/css/tv.css
-  
-  
+    node lessc /tmp/io-reader.tv.less > ../../server/client/css/tv.css
+    node lessc /tmp/io-reader.tv.less > ../../server/client-min/css/tv.css)
+  pwd
   # Compress the JS
 
   export NODE_ENV=$1

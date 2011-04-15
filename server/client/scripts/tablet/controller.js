51338e1cb5e543a437c1484141c1ee9e6e62ed3f
Eric Bidelman <ebidel@gmail.com>
2011-04-15 11:54:20 -0700

    run code after window load

--- server/client/scripts/tablet/controller.js
+++ server/client/scripts/tablet/controller.js
@@ -9,30 +9,32 @@ var TabletController = function() {
     return false;
   });
 
-  $('article > header > img.thumbnail[data-src-hi]').each(function(i, el) {
-    var $el = $(this);
+  window.addEventListener('load', function(e) {
+    // Switch out low-res thumbnails for hi-res with fade in effect.
+    $('article > header > img.thumbnail[data-src-hi]').each(function(i, el) {
+      var $el = $(this);
 
-    var srcHi = $el.data('src-hi');
-    if (srcHi) {
-      var img = new Image();
-      img.onload = function(e) {
-        var clone = el.cloneNode(true);
-        var $clone = $(clone);
-        clone.src = this.src;
-        $clone.addClass('hi-res');
-        el.insertAdjacentElement('beforeBegin', clone);
-        //$clone.insertBefore($el);
-        $clone.addClass('fadein');
+      var srcHi = $el.data('src-hi');
+      if (srcHi) {
+        var img = new Image();
+        img.onload = function(e) {
+          var clone = el.cloneNode(true);
+          var $clone = $(clone);
+          clone.src = this.src;
+          $clone.addClass('hi-res');
+          el.insertAdjacentElement('beforeBegin', clone);
+          $clone.addClass('fadein');
 
-        var transEnd = function(e) {
-          this.addEventListener('webkitTransitionEnd', transEnd, false);
-          $el.remove();
-          this.style.position = 'relative';
+          var transEnd = function(e) {
+            this.addEventListener('webkitTransitionEnd', transEnd, false);
+            $el.remove();
+            this.style.position = 'relative';
+          };
+          clone.addEventListener('webkitTransitionEnd', transEnd, false);
         };
-        clone.addEventListener('webkitTransitionEnd', transEnd, false);
-      };
-      img.src = srcHi;
-    }
+        img.src = srcHi;
+      }
+    });
   });
 
   // Start with a default category and first article selected

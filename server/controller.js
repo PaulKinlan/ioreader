bbc164465c0ed6905cbd2b6502fa526ff9a27dd4
Paul Kinlan <paulkinlan@google.com>
2011-05-02 21:12:42 +0100

    Making sure fonts are cached offline

--- server/controller.js
+++ server/controller.js
@@ -104,8 +104,6 @@ var Controller = function(configuration) {
         }
       }
 
-      console.log(data);
-
       loadTemplate(configuration.baseDir + "app.cache", function(template) {
         callback(m.to_html(template, data));
       });

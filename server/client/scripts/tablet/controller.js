a2ca1140e402bb800665ec2e9118bea89dab7cca
Eric Bidelman <ebidel@gmail.com>
2011-05-08 14:07:06 -0700

    Tablet touch complete

--- server/client/scripts/tablet/controller.js
+++ server/client/scripts/tablet/controller.js
@@ -19,6 +19,20 @@ var TabletController = function() {
   var scrollers_ = {};
 
   $(document).ready(function() {
+    // Add touch scoller on page load if an article is active.
+    var activeArticle = document.querySelector('article.active');
+    if (activeArticle && !(activeArticle.id in scrollers_)) {
+      scrollers_[activeArticle.id] = new TouchScroll(activeArticle.querySelector('section'), {elastic: true});
+
+      // Adjust story header so it'll show for load on article.
+      //$(activeArticle.querySelector('section')).css('top', '57px');
+    }
+
+    // Add touch scroller to all categories (home view).
+    if (!('categories' in scrollers_) && $('html').hasClass('menuState')) {
+      scrollers_['categories'] = new TouchScroll(document.querySelector('.categories'), {elastic: true});
+    }
+
     $('<div class="refresh"/>').insertBefore('body > header > nav').click(function() {
       controller_.refresh();
     });
@@ -41,18 +55,7 @@ var TabletController = function() {
 
   window.addEventListener('load', function(e) {
 
-    // Add touch scoller on page load if an article is active.
-    var article = document.querySelector('article.active');
-    if (article && !(article.id in scrollers_)) {
-      scrollers_[article.id] = new TouchScroll(article.querySelector('section'), {elastic: true});
-    }
-
-    /*var categories = document.querySelector('.categories');
-    if (!('categories' in scrollers_)) {
-      scrollers_['categories'] = new TouchScroll(categories, {elastic: true});
-    }*/
-
-    // Switch out low-res thumbnails for hi-res with fade in effect.
+    // Replace missing images with placeholder.
     $('img.large').each(function(i, el) {
       var $el = $(this);
       if (!$el.attr('src')) {
@@ -65,6 +68,7 @@ var TabletController = function() {
       }
     });
 
+    // Switch out low-res thumbnails for hi-res with fade in effect.
     $('article > header > img.thumbnail[data-src-hi]').each(function(i, el) {
       var $el = $(this);
 
@@ -94,9 +98,8 @@ var TabletController = function() {
 
   }, false);
 
-  // Start with a default category and first article selected
   window.addEventListener('rootchanged', function(e) {
-   /* // Get the first article.
+   /*// Start with a default category and first article selected.
     var article = $('article');
     var request = {
       params: {
@@ -105,48 +108,39 @@ var TabletController = function() {
       }
     };
     Controller_.onArticleChanged(request);*/
-console.log(e)
   });
 
   window.addEventListener('articlechanged', function(e) {
-//console.log('articlechanged', e);
+    var $articles = $('.category.active .articles');
 
-/*  var categories = document.querySelector('.category.active .articles');
-  if (!(controller_.getActiveCategory()[0].id in scrollers_)) {
-    scrollers_[controller_.getActiveCategory()[0].id] = new TouchScroll(categories, {elastic: true});
-  }
-  */
+    var opts = {
+      swipeX: function(e) {},
+      swipeY: function(e) {},
+      moveX: function(e) {},
+      moveY: function(e) {
+        var $articles = $('.category.active .articles');
+        $articles.css('marginTop', e.dy + parseInt($articles.css('marginTop')));
+      },
+      click: function(e) {
+        var $article = $(e.target).closest('article');
+        controller_.activate($article);
+      }
+    };
+    $articles.touch(opts);
 
-    // Cache touch scroller so we don't init it more than once for this el.
+    // Cache touch scroller so we don't init it more than once for this element.
     if (!(e.data.article in scrollers_)) {
-      //var div = $('article.active section').wrap('<div/>');
-      //scrollers_[e.data.article] = $('article.active section').touchScroll();
       scrollers_[e.data.article] = new TouchScroll(document.querySelector('article.active section'), {elastic: true});
     }
 
   }, false);
 
   window.addEventListener("articleready", function(e) {
-//console.log('articleready', e);
-
-    //$('article.active').touchScroll('update');
-    /*var html = $('.category.active article.active').html();
-    $('.category.active article.active').html(
-      '<iframe style="width:100%;height:100%" src="data:text/html,' + encodeURIComponent(html) + '"></iframe>');*/
-
-//var $scroller = scrollers_[e.data];
-//$('article.active .story').height($('article.active .story').height());
-//$scroller.height($scroller.attr('scrollHeight'));
-//$scroller.touchScroll('update');
-
+    //console.log('articleready', e);
   }, false);
 
   window.addEventListener("categorychanged", function(e) {
-//console.log(e, e.data.category);
-    //var top = $("#" +  e.data.category).offset().top;
-    //$(".categories").scrollTop(top);
-
-    controller_.activate($('.category.active article')); // Activate first article.
+    controller_.activate($('.category.active article')); // Activate 1st article.
   }, false);
 
 };
